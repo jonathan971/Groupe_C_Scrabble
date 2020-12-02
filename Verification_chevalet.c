cbe79c45369ba90e4c78a5f0119a36275ef28799
PaulNavon <paul.navon@edu.ece.fr>
2020-12-02 14:20:14 +0100

    On travaille sur le placement du mot

--- Verification_chevalet.c
+++ Verification_chevalet.c
@@ -31,8 +31,10 @@ void placementMot(char plateau_de_jeu[MAX][MAX], char chevalet_joueur[MAX_DECK])
 
 int nombreDeLettres(){
     int nombreLettres = 0;
-    printf("Combien de lettres souhaitez-vous placer?\n");
-    scanf("%d", &nombreLettres);
+    do{
+        printf("Combien de lettres souhaitez-vous placer?\n");
+        scanf("%d", &nombreLettres);
+    }while(nombreLettres<0 || nombreLettres>7);
     return nombreLettres;
 }
 
@@ -50,11 +52,11 @@ void placementPremiereLettrePremierMot(int nombreLettres, char plateau_de_jeu[MA
     int k = 0;
     do { //verification que la lettre est presente sur le chevalet
         do{ //verification que le caractere est bien une lettre
-            printf("Vous commencez la partie\n. Saisissez la lettre que vous souhaitez poser sur la case du milieu:\n");
+            printf("Vous commencez la partie.\nSaisissez la lettre que vous souhaitez poser sur la case du milieu:\n");
             scanf(" %c", &lettreAPlacer);
             for (k = 0; k < nombreLettres; k++) {
                 if (lettreAPlacer == chevalet_joueur[k]) {
-                    plateau_de_jeu[8][8] = lettreAPlacer;
+                    plateau_de_jeu[7][7] = lettreAPlacer;
                     chevalet_joueur[k] = '0'; //suppression de la lettre utilisee pour ne pas pouvoir la remettre une autre fois
                 }
             }
@@ -62,10 +64,10 @@ void placementPremiereLettrePremierMot(int nombreLettres, char plateau_de_jeu[MA
                 printf("Le caractere saisi ne correspond pas a une lettre. Veuillez recommencer:\n");
             }
         } while (islower(lettreAPlacer) == 0 && isupper(lettreAPlacer == 0));
-        if (plateau_de_jeu[8][8] == ' ') {
+        if (plateau_de_jeu[7][7] == (char)32) {
             printf("Le caractere saisi ne correspond pas aux lettres sur votre chevalet. Veuillez recommencer:\n");
         }
-    } while (plateau_de_jeu[8][8] == ' ');
+    } while (plateau_de_jeu[7][7] == (char)32);
 }
 
 void placementPremiereLettre(char plateau_de_jeu[MAX][MAX]){
@@ -73,28 +75,35 @@ void placementPremiereLettre(char plateau_de_jeu[MAX][MAX]){
     do {
         printf("Saisissez la case dans laquelle vous souhaitez commencer votre mot:\n");
         scanf("%d %d", &i, &j);
-        if (plateau_de_jeu[i][j] != ' ') {
+        i+=1;
+        j+=1;
+        if (plateau_de_jeu[i][j] != (char)32||plateau_de_jeu[i][j] != (char)38|| plateau_de_jeu[i][j] != (char)64
+            || plateau_de_jeu[i][j] != (char)37 || plateau_de_jeu[i][j] != (char)36) {
             printf("La case que vous avez saisie est deja prise. Veuillez recommencer.\n");
         }
-    }while (plateau_de_jeu[i][j] != ' ');
+    }while (plateau_de_jeu[i][j] != (char)32||plateau_de_jeu[i][j] != (char)38|| plateau_de_jeu[i][j] != (char)64
+            || plateau_de_jeu[i][j] != (char)37 || plateau_de_jeu[i][j] != (char)36);
 }
 
 void placementVertical(int nombreLettres, char plateau_de_jeu[MAX][MAX], char chevalet_joueur[MAX_DECK]){
     int i = 0, j = 0;
     while (nombreLettres > 0) {
         do {
-            printf("Saisissez le numero de la case suivante (deuxieme composant de la case)");
+            printf("Saisissez le numero de la case suivante (deuxieme composant de la case)\n");
             scanf("%d", &j); //[j] pour que le mot soit ecrit vers le bas comme [i] ne change pas
-            if (plateau_de_jeu[i][j] != ' ') {
+            j+=1;
+            if (plateau_de_jeu[i][j] != (char)32||plateau_de_jeu[i][j] != (char)38|| plateau_de_jeu[i][j] != (char)64
+                || plateau_de_jeu[i][j] != (char)37 || plateau_de_jeu[i][j] != (char)36) {
                 printf("La case que vous avez saisie est deja prise. Veuillez recommencer.\n");
             }
-        }while (plateau_de_jeu[i][j] != ' ');
+        }while (plateau_de_jeu[i][j] != (char)32||plateau_de_jeu[i][j] != (char)38|| plateau_de_jeu[i][j] != (char)64
+        || plateau_de_jeu[i][j] != (char)37 || plateau_de_jeu[i][j] != (char)36);
         placementAutreLettre(nombreLettres, i, j, plateau_de_jeu, chevalet_joueur);
         nombreLettres--;
     }
 }
 void placementAutreLettre(int nombreLettres, int i, int j, char plateau_de_jeu[MAX][MAX], char chevalet_joueur[MAX_DECK]){
-    int k= 0;
+    int k;
     char lettreAPlacer = '0';
     do { //verification que la lettre est presente sur le chevalet
         do{ //verification que le caractere est bien une lettre
@@ -110,22 +119,27 @@ void placementAutreLettre(int nombreLettres, int i, int j, char plateau_de_jeu[M
                 printf("Le caractere saisi ne correspond pas a une lettre. Veuillez recommencer:\n");
             }
         }while (islower(lettreAPlacer) == 0 && isupper(lettreAPlacer == 0));
-        if (plateau_de_jeu[i][j] == ' ') {
+        if (plateau_de_jeu[i][j] != (char)32||plateau_de_jeu[i][j] != (char)38|| plateau_de_jeu[i][j] != (char)64
+            || plateau_de_jeu[i][j] != (char)37 || plateau_de_jeu[i][j] != (char)36) {
             printf("La saisie ne correspond pas aux lettres sur votre chevalet. Veuillez recommencer:\n");
         }
-    }while (plateau_de_jeu[i][j] == ' ');
+    }while (plateau_de_jeu[i][j] != (char)32||plateau_de_jeu[i][j] != (char)38|| plateau_de_jeu[i][j] != (char)64
+            || plateau_de_jeu[i][j] != (char)37 || plateau_de_jeu[i][j] != (char)36);
 }
 
 void placementHorizontal(int nombreLettres, char plateau_de_jeu[MAX][MAX], char chevalet_joueur[MAX_DECK]){
     int i = 0, j = 0;
     while (nombreLettres > 0) {
         do {
-            printf("Saisissez le numero de la case suivante (premier composant de la case (chiffre horizontal))");
+            printf("Saisissez le numero de la case suivante (premier composant de la case (chiffre horizontal))\n");
             scanf("%d", &i); //[i] pour que le mot soit ecrit vers le bas comme [j] ne change pas
-            if (plateau_de_jeu[i][j] != ' ') {
+            i+=1;
+            if (plateau_de_jeu[i][j] != (char)32||plateau_de_jeu[i][j] != (char)38|| plateau_de_jeu[i][j] != (char)64
+                || plateau_de_jeu[i][j] != (char)37 || plateau_de_jeu[i][j] != (char)36) {
                 printf("La case que vous avez saisie est deja prise. Veuillez recommencer.\n");
             }
-        }while (plateau_de_jeu[i][j] != ' ');
+        }while (plateau_de_jeu[i][j] != (char)32||plateau_de_jeu[i][j] != (char)38|| plateau_de_jeu[i][j] != (char)64
+                || plateau_de_jeu[i][j] != (char)37 || plateau_de_jeu[i][j] != (char)36);
         placementAutreLettre(nombreLettres, i, j, plateau_de_jeu, chevalet_joueur);
         nombreLettres--;
     }
